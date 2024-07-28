ce85c5743dcb34fb4d309017c14455e3708c7614
SohailKhan <mdsohailkhan2267@gmail.com>
2024-07-29 00:26:45 +0530

    Modify multer config

--- server/Utils/Multer.js
+++ server/Utils/Multer.js
@@ -2,13 +2,21 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const tempDir = path.join(__dirname, '../public/temp');
+
+// Ensure the temp directory exists
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/temp'));
+        cb(null, tempDir);
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
