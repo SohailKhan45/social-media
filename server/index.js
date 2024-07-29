dea26ab1399916ca191c1f97f367927515cdea4f
SohailKhan <mdsohailkhan2267@gmail.com>
2024-07-29 18:20:45 +0530

    Fix post like bug

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [FRONTEND_URL, 'http://localhost:3000'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
